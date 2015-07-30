6a5762f08102ef9205a68692d2c05aefa68bed63
Thomas Burleson <ThomasBurleson@Gmail.com>
2015-07-30 11:03:54 -0500

    tests(dialog): update notes for `triggerAnimation()`

--- src/components/dialog/dialog.spec.js
+++ src/components/dialog/dialog.spec.js
@@ -1,4 +1,4 @@
-describe('$mdDialog', function() {
+ddescribe('$mdDialog', function() {
   var triggerAnimation;
 
   beforeEach(module('material.components.dialog'));
@@ -16,13 +16,9 @@ describe('$mdDialog', function() {
   beforeEach(inject(function($rootScope, $timeout, $$rAF){
 
     triggerAnimation = function() {
-      try {
-        $timeout.flush();
-        $rootScope.$apply();
-        $$rAF.flush();
-      } finally {
-        $timeout.flush();
-      }
+        $timeout.flush(); // flush to start animations
+        $$rAF.flush();    // flush animations
+        $timeout.flush(); // flush responses after animation completions
     }
   }));
 
